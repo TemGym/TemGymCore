fd48a058de2b7606894c074fb1d8a2ed5d819410
DavidLanders95 <davidlanders2009@hotmail.com>
2025-09-04 16:16:35 +0200

    map reduce added to make it go brrrrrrr

--- src/temgym_core/gaussian.py
+++ src/temgym_core/gaussian.py
@@ -4,6 +4,9 @@ from .grid import Grid
 from .run import run_to_end
 from .utils import custom_jacobian_matrix
 from dataclasses import fields
+from jax._src.lax.control_flow.loops import _batch_and_remainder
+from jax import lax
+
 
 def w_z(w0, z, z_r):
     return w0 * jnp.sqrt(1 + (z / z_r) ** 2)
@@ -129,25 +132,26 @@ def _beam_field(
     return pref * jnp.exp(1j * phase)    # (np,)
 
 
-@jax.jit
 def propagate_misaligned_gaussian_jax_scan(
-    amp, Q1_inv, A, B, C, D, e, r2, r1m, theta1m, k
+    amp, Q1_inv, A, B, C, D, e, r2, r1m, theta1m, k, batch_size=128
 ):
     npix = r2.shape[0]
     Q2_inv = Qinv_ABCD(Q1_inv, A, B, C, D)  # (nb,2,2)
 
-    def body(acc, xs):
+    def _beam_field_outer(xs):
         a_i, q1_i, q2_i, r1_i, t1_i, A_i, B_i, e_i, k_i = xs
-        f = _beam_field(a_i, q1_i, q2_i, r2, r1_i, t1_i, k_i, A_i, B_i, e_i)  # (npix,)
-        return acc + f, None
+        return _beam_field(a_i, q1_i, q2_i, r2, r1_i, t1_i, k_i, A_i, B_i, e_i)
 
     init = jnp.zeros((npix,), dtype=jnp.complex128)
     xs = (amp, Q1_inv, Q2_inv, r1m, theta1m, A, B, e, k)
-    out, _ = jax.lax.scan(body, init, xs)
+    out = map_reduce(_beam_field_outer, jnp.add, init, xs, batch_size=batch_size)
     return out  # (npix,)
 
 
-def get_image(gaussian_rays, model):
+propagate_misaligned_gaussian_jax_scan = jax.jit(propagate_misaligned_gaussian_jax_scan, static_argnames=['batch_size'])
+
+
+def get_image(gaussian_rays, model, batch_size=128):
     from .ray import GaussianRay
 
     rays = gaussian_rays
@@ -183,6 +187,39 @@ def get_image(gaussian_rays, model):
     k = 2 * jnp.pi / wavelengths
 
     output_field = propagate_misaligned_gaussian_jax_scan(
-        amplitudes, Q1_invs, As, Bs, Cs, Ds, es, r2, r1ms, theta1ms, k
+        amplitudes, Q1_invs, As, Bs, Cs, Ds, es, r2, r1ms, theta1ms, k, batch_size=batch_size
     ).reshape(grid.shape)
     return output_field
+
+
+def map_reduce(f, reducer, init, xs, *, batch_size: int | None = None):
+
+  def scan_fn(acc_inner, x):
+    # combine f and reducer into function appropriate for normal lax.scan in reduce-only mode
+    return reducer(acc_inner, f(x)), None
+
+  if batch_size is not None:
+    scan_xs, remainder_xs = _batch_and_remainder(xs, batch_size)
+
+    def reduce_chunk(acc, x):
+      # Reduce x into acc, assuming x have already been f'd
+      return reducer(acc, x), None
+
+    def map_reduce_chunk(acc, x):
+      #  Vmap apply f to a chunk of x's, then reduce them sequentially into acc
+      elements = jax.vmap(f)(x)
+      return lax.scan(reduce_chunk, acc, elements)
+
+    if scan_xs is not None:
+      # Map f over each chunk of xs, and reduce each sequentially into init
+      acc, _ = lax.scan(map_reduce_chunk, init, scan_xs)
+    else:
+      acc, _ = init, None
+
+    if remainder_xs is not None:
+      # normal scan-reduce the remainder chunk into acc (could also be vmapped?)
+      acc, _ = lax.scan(scan_fn, acc, remainder_xs)
+  else:
+    # normal scan-reduce the remainder chunk into acc (could also be vmapped?)
+    acc, _ = lax.scan(scan_fn, init, xs)
+  return acc
